de8d40b16a47a43e5ab17537ada2b50f9af56368
kayoGc <kayovictorgoncaloc@hotmail.com>
2025-12-01 19:15:20 -0300

    Day 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Solutions-AoC-2025
+
+Just doing some puzzles for the fun of it! All solutions are made with JS.
+
+## Advent of Code
+
+To learn more about the puzzles see this website: https://adventofcode.com/2025/about
---
+++ day_1/part_1.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path')
+
+// solution - advent of code - day 1 - part 1
+function solution(input, current_number = 50) {
+    const input_arr = input.split('\n');
+
+    let password = 0;
+
+    for (const input of input_arr) {
+        const char = input[0];
+        let number = parseInt(input.substring(1));
+
+        for (let i = 0; i < number; i++) {
+
+            if (char === 'L') {
+                current_number--;
+                
+                if (current_number < 0) {
+                    current_number = 99;
+                } 
+            }
+
+            if (char === 'R') {
+                current_number++;
+
+                if (current_number > 99) {
+                    current_number = 0;
+                }
+            }
+        }
+        
+        if (current_number === 0) {
+            password++;
+        }
+    }
+
+    console.log(`The password is: ${password}`);
+}
+
+// reads the input
+fs.readFile(path.resolve(__dirname, 'input'), 'utf-8', (err, data) => {
+
+    if (err) {
+        console.error("Error reading the input:", err);
+        return;
+    }
+
+    solution(data);
+})
